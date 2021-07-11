c9717420f3db911c045db8b50c87202725dd046a
soregashi-27 <tomo.soul.15@gmail.com>
2021-07-11 15:18:31 +0900

    他のRepositoryはこちらを追加

---
+++ src/components/sections/projects.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'gatsby';
+import styled from 'styled-components';
+
+const StyledProjectsSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 24px;
+  }
+`;
+
+const Project = () => {
+  return (
+    <StyledProjectsSection>
+      <h2>他のGithub Repositoryはこちら</h2>
+      <a href="https://github.com/soregashi-27">View my Github?</a>
+      {/* Repositoryが増えてきたらaタグをLinkタグにしてページを追加する */}
+    </StyledProjectsSection>
+  );
+};
+
+export default Project;
