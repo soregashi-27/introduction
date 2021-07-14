d07a32464dc36bb2209ec9922c36bef41d836c0f
soregashi-27 <tomo.soul.15@gmail.com>
2021-07-14 22:31:34 +0900

    experience.jsを追加

--- src/components/sections/experience.js
+++ src/components/sections/experience.js
@@ -5,10 +5,15 @@ const StyledExperienceSection = styled.section`
   max-width: 700px;
 `;
 
+const StyledTabList = styled.div``;
+
 const Experience = () => {
   return (
     <StyledExperienceSection>
       <h2>Experience</h2>
+      <div className="inner">
+        <StyledTabList>// ここにタップしたら表示が変わるTab Listを追加していく</StyledTabList>
+      </div>
     </StyledExperienceSection>
   );
 };
